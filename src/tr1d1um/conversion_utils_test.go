d80a2583045df7ceae8804f6841df4b3c269d650
Weston Schmidt <Weston_Schmidt@alumni.purdue.edu>
2017-09-15 12:31:54 -0700

    Run gofmt across the code.

--- src/tr1d1um/conversion_utils_test.go
+++ src/tr1d1um/conversion_utils_test.go
@@ -1,58 +1,57 @@
 package main
 
 import (
-	"testing"
-	"encoding/json"
-	"github.com/stretchr/testify/assert"
-	"net/http"
-	"strings"
-	"io"
-	"errors"
-	"net/http/httptest"
 	"bytes"
+	"encoding/json"
+	"errors"
+	"github.com/stretchr/testify/assert"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
 )
 
-var sampleNames = []string{"p1","p2"}
+var sampleNames = []string{"p1", "p2"}
 
 func TestGetFlavorFormat(t *testing.T) {
 	assert := assert.New(t)
 
-	t.Run("IdealGet", func (t *testing.T) {
+	t.Run("IdealGet", func(t *testing.T) {
 		req := httptest.NewRequest(http.MethodGet, "http://api/device/config?names=p1,p2", nil)
 
-		wdmp := &GetWDMP{Command:COMMAND_GET, Names:sampleNames}
+		wdmp := &GetWDMP{Command: COMMAND_GET, Names: sampleNames}
 
 		expected, expectedErr := json.Marshal(wdmp)
-		actual, actualErr := GetFlavorFormat(req,"attributes", "names", ",")
+		actual, actualErr := GetFlavorFormat(req, "attributes", "names", ",")
 
 		assert.EqualValues(expected, actual)
 		assert.EqualValues(expectedErr, actualErr)
 	})
 
-	t.Run("IdealGetAttr", func (t *testing.T){
+	t.Run("IdealGetAttr", func(t *testing.T) {
 		req := httptest.NewRequest(http.MethodGet, "http://api/device/config?names=p1,p2&attributes=attr1",
 			nil)
 
-		wdmp := &GetWDMP{Command:COMMAND_GET_ATTRS, Names:sampleNames, Attribute:"attr1"}
+		wdmp := &GetWDMP{Command: COMMAND_GET_ATTRS, Names: sampleNames, Attribute: "attr1"}
 
 		expected, expectedErr := json.Marshal(wdmp)
-		actual, actualErr := GetFlavorFormat(req,"attributes", "names", ",")
+		actual, actualErr := GetFlavorFormat(req, "attributes", "names", ",")
 
 		assert.EqualValues(expected, actual)
 		assert.EqualValues(expectedErr, actualErr)
 	})
 
-	t.Run("NoNames", func (t *testing.T){
+	t.Run("NoNames", func(t *testing.T) {
 		req := httptest.NewRequest(http.MethodGet, "http://api/device/config?names=",
 			nil)
 
-		_ , err := GetFlavorFormat(req,"attributes", "names", ",")
+		_, err := GetFlavorFormat(req, "attributes", "names", ",")
 
 		assert.NotNil(err)
 		assert.True(strings.HasPrefix(err.Error(), "names is a required"))
 	})
 }
-
 
 func TestAddFlavorFormat(t *testing.T) {
 
@@ -62,36 +61,35 @@ func TestDeleteFlavorFormat(t *testing.T) {
 
 }
 
-
 func TestReplaceFlavorFormat(t *testing.T) {
 	assert := assert.New(t)
 	cReader := CustomResultReader{}
 	commonVars := Vars{"uThere?": "yes!"}
 	emptyVars := Vars{}
 
-	t.Run("TableNotProvided", func (t *testing.T){
+	t.Run("TableNotProvided", func(t *testing.T) {
 		_, err := ReplaceFlavorFormat(nil, emptyVars, "uThere?", cReader.CustomReader)
 		assert.NotNil(err)
 		assert.True(strings.HasPrefix(err.Error(), "tableName"))
 	})
-	
-	t.Run("DecodeJsonErr", func (t *testing.T) {
+
+	t.Run("DecodeJsonErr", func(t *testing.T) {
 		cReader.data = []byte("")
 		_, err := ReplaceFlavorFormat(nil, commonVars, "uThere?", cReader.CustomReader)
 		assert.NotNil(err)
 		assert.True(strings.HasPrefix(err.Error(), "JSON"))
 	})
-	
-	t.Run("InvalidParams", func (t *testing.T) {
+
+	t.Run("InvalidParams", func(t *testing.T) {
 		cReader.data = []byte("{}")
 		_, err := ReplaceFlavorFormat(nil, commonVars, "uThere?", cReader.CustomReader)
 		assert.NotNil(err)
 		assert.True(strings.HasPrefix(err.Error(), "invalid"))
 	})
 
-	t.Run("IdealPath", func (t *testing.T) {
-		wdmpObj := &ReplaceRowsWDMP{Command:COMMAND_REPLACE_ROWS, Table: commonVars["uThere?"]}
-		wdmpObj.Rows = map[string]map[string]string{"0":{"uno":"one", "dos":"two"}}
+	t.Run("IdealPath", func(t *testing.T) {
+		wdmpObj := &ReplaceRowsWDMP{Command: COMMAND_REPLACE_ROWS, Table: commonVars["uThere?"]}
+		wdmpObj.Rows = map[string]map[string]string{"0": {"uno": "one", "dos": "two"}}
 		cReader.data = []byte(`{"0":{"uno":"one","dos":"two"}}`)
 
 		actual, actualErr := ReplaceFlavorFormat(nil, commonVars, "uThere?", cReader.CustomReader)
@@ -105,9 +103,9 @@ func TestReplaceFlavorFormat(t *testing.T) {
 func TestGetFromUrlPath(t *testing.T) {
 	assert := assert.New(t)
 
-	fakeUrlVar := map[string]string{"k1":"k1v1,k1v2", "k2": "k2v1"}
+	fakeUrlVar := map[string]string{"k1": "k1v1,k1v2", "k2": "k2v1"}
 
-	t.Run("NormalCases", func (t *testing.T){
+	t.Run("NormalCases", func(t *testing.T) {
 
 		k1ValGroup, exists := GetFromUrlPath("k1", fakeUrlVar)
 		assert.True(exists)
@@ -118,12 +116,12 @@ func TestGetFromUrlPath(t *testing.T) {
 		assert.EqualValues("k2v1", k2ValGroup)
 	})
 
-	t.Run("NonNilNonExistent", func (t *testing.T){
+	t.Run("NonNilNonExistent", func(t *testing.T) {
 		_, exists := GetFromUrlPath("k3", fakeUrlVar)
 		assert.False(exists)
 	})
 
-	t.Run("NilCase", func (t *testing.T){
+	t.Run("NilCase", func(t *testing.T) {
 		_, exists := GetFromUrlPath("k", nil)
 		assert.False(exists)
 	})
@@ -134,15 +132,15 @@ func TestDecodeJsonPayload(t *testing.T) {
 
 	cReader := CustomResultReader{}
 
-	t.Run("ReadEntireBodyFails", func (t *testing.T) {
+	t.Run("ReadEntireBodyFails", func(t *testing.T) {
 		cReader.err = errors.New("fail")
 		err := DecodeJsonPayload(nil, nil, cReader.CustomReader)
 
 		assert.NotNil(err)
-		assert.EqualValues(err.Error(),"fail")
+		assert.EqualValues(err.Error(), "fail")
 	})
 
-	t.Run("EmptyInput", func (t *testing.T) {
+	t.Run("EmptyInput", func(t *testing.T) {
 		emptyBody := bytes.NewBufferString("")
 		cReader.data, cReader.err = []byte(""), nil
 
@@ -152,10 +150,10 @@ func TestDecodeJsonPayload(t *testing.T) {
 		assert.EqualValues(err, ErrJsonEmpty)
 	})
 
-	t.Run("IdealPath", func (t *testing.T) {
+	t.Run("IdealPath", func(t *testing.T) {
 		cReader.data = []byte(`{"0":"zero","1":"one"}`)
 
-		expected := map[string]string{"0":"zero", "1":"one"}
+		expected := map[string]string{"0": "zero", "1": "one"}
 		actual := make(map[string]string)
 
 		err := DecodeJsonPayload(nil, &actual, cReader.CustomReader)
@@ -167,10 +165,9 @@ func TestDecodeJsonPayload(t *testing.T) {
 
 type CustomResultReader struct {
 	data []byte
-	err error
+	err  error
 }
 
-func(c CustomResultReader) CustomReader (_ io.Reader)([]byte,error){
+func (c CustomResultReader) CustomReader(_ io.Reader) ([]byte, error) {
 	return c.data, c.err
 }
-
