6a3f918a7b5662edb0d7ab9b91d8238da2c77afd
Weston Schmidt <Weston_Schmidt@alumni.purdue.edu>
2017-10-26 16:53:55 -0700

    Clean up the CLA and add headers to all the files.  Also add the NOTICE file.

--- src/tr1d1um/http_test.go
+++ src/tr1d1um/http_test.go
@@ -1,3 +1,20 @@
+/**
+ * Copyright 2017 Comcast Cable Communications Management, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package main
 
 import (
@@ -119,7 +136,7 @@ func TestForwardHeadersByPrefix(t *testing.T) {
 	t.Run("MultipleHeadersFiltered", func(t *testing.T) {
 		assert := assert.New(t)
 		origin := httptest.NewRecorder()
-		resp := &http.Response{Header:http.Header{}}
+		resp := &http.Response{Header: http.Header{}}
 
 		resp.Header.Add("Helium", "3")
 		resp.Header.Add("Hydrogen", "5")
@@ -134,7 +151,7 @@ func TestForwardHeadersByPrefix(t *testing.T) {
 	t.Run("MultipleHeadersFilteredFullArray", func(t *testing.T) {
 		assert := assert.New(t)
 		origin := httptest.NewRecorder()
-		resp := &http.Response{Header:http.Header{}}
+		resp := &http.Response{Header: http.Header{}}
 
 		resp.Header.Add("Helium", "3")
 		resp.Header.Add("Hydrogen", "5")
@@ -143,7 +160,7 @@ func TestForwardHeadersByPrefix(t *testing.T) {
 		ForwardHeadersByPrefix("H", origin, resp)
 		assert.NotEmpty(origin.Header())
 		assert.EqualValues(2, len(origin.Header()))
-		assert.EqualValues([]string{"5","6"}, origin.Header()["Hydrogen"])
+		assert.EqualValues([]string{"5", "6"}, origin.Header()["Hydrogen"])
 	})
 }
 
