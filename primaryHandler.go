f43db589f0568c63954389fa38088b0c4aa1dd47
mtrinh11 <mtrinh11@gmail.com>
2022-02-24 09:12:50 -0800

    Prev version compatible (#253)
    * added previous version capatibility
    
    * fixed viper call
    
    * changed naming and fixed named types for argus and xmidt timeouts
    
    * replaced all viper calls for configuration
    
    * fixed config key issues

--- primaryHandler.go
+++ primaryHandler.go
@@ -34,6 +34,7 @@ import (
 	"github.com/justinas/alice"
 	"github.com/spf13/viper"
 	"github.com/xmidt-org/ancla"
+	"github.com/xmidt-org/arrange"
 	"github.com/xmidt-org/arrange/arrangehttp"
 	"github.com/xmidt-org/bascule"
 	"github.com/xmidt-org/bascule/acquire"
@@ -106,67 +107,70 @@ func newHTTPClient(timeouts httpClientTimeout, tracing candlelight.Tracing) *htt
 	}
 }
 
-func createAuthAcquirer(v *viper.Viper) (acquire.Acquirer, error) {
-	var options authAcquirerConfig
-	err := v.UnmarshalKey(authAcquirerKey, &options)
+type createAuthAcquirerIn struct {
+	fx.In
+	AuthAcquirer authAcquirerConfig `name:"authAcquirer"`
+}
 
-	if err != nil {
-		return nil, err
+func createAuthAcquirer(in createAuthAcquirerIn) (acquire.Acquirer, error) {
+	if in.AuthAcquirer.JWT.AuthURL != "" && in.AuthAcquirer.JWT.Buffer != 0 && in.AuthAcquirer.JWT.Timeout != 0 {
+		return acquire.NewRemoteBearerTokenAcquirer(in.AuthAcquirer.JWT)
 	}
 
-	if options.JWT.AuthURL != "" && options.JWT.Buffer != 0 && options.JWT.Timeout != 0 {
-		return acquire.NewRemoteBearerTokenAcquirer(options.JWT)
-	}
-
-	if options.Basic != "" {
-		return acquire.NewFixedAuthAcquirer(options.Basic)
+	if in.AuthAcquirer.Basic != "" {
+		return acquire.NewFixedAuthAcquirer(in.AuthAcquirer.Basic)
 	}
 
 	return nil, errors.New("auth acquirer not configured properly")
 }
 
+type provideAuthenticationIn struct {
+	fx.In
+	Logger          log.Logger
+	Registry        xmetrics.Registry
+	AuthHeader      []string `name:"authHeader"`
+	JWTVal          JWTValidator
+	CapabilityCheck CapabilityConfig
+}
+
 // authenticationHandler configures the authorization requirements for requests to reach the main handler
 //
 //nolint:funlen
-func authenticationProvider(v *viper.Viper, logger log.Logger, registry xmetrics.Registry) (*alice.Chain, error) {
-	if registry == nil {
+func provideAuthentication(in provideAuthenticationIn) (*alice.Chain, error) {
+	if in.Registry == nil {
 		return nil, errors.New("nil registry")
 	}
 
-	basculeMeasures := basculemetrics.NewAuthValidationMeasures(registry)
-	capabilityCheckMeasures := basculechecks.NewAuthCapabilityCheckMeasures(registry)
+	basculeMeasures := basculemetrics.NewAuthValidationMeasures(in.Registry)
+	capabilityCheckMeasures := basculechecks.NewAuthCapabilityCheckMeasures(in.Registry)
 	listener := basculemetrics.NewMetricListener(basculeMeasures)
 
 	basicAllowed := make(map[string]string)
-	basicAuth := v.GetStringSlice("authHeader")
-	for _, a := range basicAuth {
+	for _, a := range in.AuthHeader {
 		decoded, err := base64.StdEncoding.DecodeString(a)
 		if err != nil {
-			logging.Info(logger).Log(logging.MessageKey(), "failed to decode auth header", "authHeader", a, logging.ErrorKey(), err.Error())
+			logging.Info(in.Logger).Log(logging.MessageKey(), "failed to decode auth header", "authHeader", a, logging.ErrorKey(), err.Error())
 			continue
 		}
 
 		i := bytes.IndexByte(decoded, ':')
-		logging.Debug(logger).Log(logging.MessageKey(), "decoded string", "string", decoded, "i", i)
+		logging.Debug(in.Logger).Log(logging.MessageKey(), "decoded string", "string", decoded, "i", i)
 		if i > 0 {
 			basicAllowed[string(decoded[:i])] = string(decoded[i+1:])
 		}
 	}
-	logging.Debug(logger).Log(logging.MessageKey(), "Created list of allowed basic auths", "allowed", basicAllowed, "config", basicAuth)
+	logging.Debug(in.Logger).Log(logging.MessageKey(), "Created list of allowed basic auths", "allowed", basicAllowed, "config", in.AuthHeader)
 
 	options := []basculehttp.COption{
 		basculehttp.WithCLogger(getLogger),
 		basculehttp.WithCErrorResponseFunc(listener.OnErrorResponse),
-		basculehttp.WithParseURLFunc(basculehttp.CreateRemovePrefixURLFunc("/"+apiBase+"/", basculehttp.DefaultParseURLFunc)),
 	}
 	if len(basicAllowed) > 0 {
 		options = append(options, basculehttp.WithTokenFactory("Basic", basculehttp.BasicTokenFactory(basicAllowed)))
 	}
-	var jwtVal JWTValidator
 
-	v.UnmarshalKey("jwtValidator", &jwtVal)
-	if jwtVal.Keys.URI != "" {
-		resolver, err := jwtVal.Keys.NewResolver()
+	if in.JWTVal.Keys.URI != "" {
+		resolver, err := in.JWTVal.Keys.NewResolver()
 		if err != nil {
 			return &alice.Chain{}, emperror.With(err, "failed to create resolver")
 		}
@@ -175,11 +179,17 @@ func authenticationProvider(v *viper.Viper, logger log.Logger, registry xmetrics
 			DefaultKeyID: DefaultKeyID,
 			Resolver:     resolver,
 			Parser:       bascule.DefaultJWTParser,
-			Leeway:       jwtVal.Leeway,
+			Leeway:       in.JWTVal.Leeway,
 		}))
 	}
 
-	authConstructor := basculehttp.NewConstructor(options...)
+	authConstructor := basculehttp.NewConstructor(append([]basculehttp.COption{
+		basculehttp.WithParseURLFunc(basculehttp.CreateRemovePrefixURLFunc("/"+apiBase+"/", basculehttp.DefaultParseURLFunc)),
+	}, options...)...)
+	authConstructorLegacy := basculehttp.NewConstructor(append([]basculehttp.COption{
+		basculehttp.WithParseURLFunc(basculehttp.CreateRemovePrefixURLFunc("/api/"+prevAPIVersion+"/", basculehttp.DefaultParseURLFunc)),
+		basculehttp.WithCErrorHTTPResponseFunc(basculehttp.LegacyOnErrorHTTPResponse),
+	}, options...)...)
 
 	bearerRules := bascule.Validators{
 		bchecks.NonEmptyPrincipal(),
@@ -188,18 +198,16 @@ func authenticationProvider(v *viper.Viper, logger log.Logger, registry xmetrics
 	}
 
 	// only add capability check if the configuration is set
-	var capabilityCheck CapabilityConfig
-	v.UnmarshalKey("capabilityCheck", &capabilityCheck)
-	if capabilityCheck.Type == "enforce" || capabilityCheck.Type == "monitor" {
+	if in.CapabilityCheck.Type == "enforce" || in.CapabilityCheck.Type == "monitor" {
 		var endpoints []*regexp.Regexp
-		c, err := basculechecks.NewEndpointRegexCheck(capabilityCheck.Prefix, capabilityCheck.AcceptAllMethod)
+		c, err := basculechecks.NewEndpointRegexCheck(in.CapabilityCheck.Prefix, in.CapabilityCheck.AcceptAllMethod)
 		if err != nil {
 			return nil, emperror.With(err, "failed to create capability check")
 		}
-		for _, e := range capabilityCheck.EndpointBuckets {
+		for _, e := range in.CapabilityCheck.EndpointBuckets {
 			r, err := regexp.Compile(e)
 			if err != nil {
-				logging.Error(logger).Log(logging.MessageKey(), "failed to compile regular expression", "regex", e, logging.ErrorKey(), err.Error())
+				logging.Error(in.Logger).Log(logging.MessageKey(), "failed to compile regular expression", "regex", e, logging.ErrorKey(), err.Error())
 				continue
 			}
 			endpoints = append(endpoints, r)
@@ -209,7 +217,7 @@ func authenticationProvider(v *viper.Viper, logger log.Logger, registry xmetrics
 			Measures:  capabilityCheckMeasures,
 			Endpoints: endpoints,
 		}
-		bearerRules = append(bearerRules, m.CreateValidator(capabilityCheck.Type == "enforce"))
+		bearerRules = append(bearerRules, m.CreateValidator(in.CapabilityCheck.Type == "enforce"))
 	}
 
 	authEnforcer := basculehttp.NewEnforcer(
@@ -220,13 +228,25 @@ func authenticationProvider(v *viper.Viper, logger log.Logger, registry xmetrics
 		basculehttp.WithRules("Bearer", bearerRules),
 		basculehttp.WithEErrorResponseFunc(listener.OnErrorResponse),
 	)
-	constructors := []alice.Constructor{setLogger(logger), authConstructor, authEnforcer, basculehttp.NewListenerDecorator(listener)}
 
-	chain := alice.New(constructors...)
-	return &chain, nil
+	authChain := alice.New(setLogger(in.Logger), authConstructor, authEnforcer, basculehttp.NewListenerDecorator(listener))
+	authChainLegacy := alice.New(setLogger(in.Logger), authConstructorLegacy, authEnforcer, basculehttp.NewListenerDecorator(listener))
+	versionCompatibleAuth := alice.New(func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(r http.ResponseWriter, req *http.Request) {
+			vars := mux.Vars(req)
+			if vars != nil {
+				if vars["version"] == prevAPIVersion {
+					authChainLegacy.Then(next).ServeHTTP(r, req)
+					return
+				}
+			}
+			authChain.Then(next).ServeHTTP(r, req)
+		})
+	})
+	return &versionCompatibleAuth, nil
 }
 
-type webhookHandlerIn struct {
+type handleWebhooksIn struct {
 	fx.In
 	V                  viper.Viper
 	WebhookConfigKey   ancla.Config
@@ -234,11 +254,15 @@ type webhookHandlerIn struct {
 	Logger             log.Logger
 	MetricsRegistry    xmetrics.Registry
 	Tracing            candlelight.Tracing
-	APIRouter          *mux.Router
+	APIRouter          *mux.Router `name:"api_router"`
 	Authenticate       *alice.Chain
 }
 
-func webhookHandler(in webhookHandlerIn) error {
+type handleWebhooksOut struct {
+	fx.Out
+}
+
+func handleWebhooks(in handleWebhooksIn) (out handleWebhooksOut, err error) {
 	//
 	// Webhooks (if not configured, handlers are not set up)
 	//
@@ -252,12 +276,12 @@ func webhookHandler(in webhookHandlerIn) error {
 
 		svc, _, err := ancla.Initialize(webhookConfig, getLogger, logging.WithLogger)
 		if err != nil {
-			return fmt.Errorf("failed to initialize webhook service: %s", err)
+			return out, fmt.Errorf("failed to initialize webhook service: %s", err)
 		}
 
 		builtValidators, err := ancla.BuildValidators(webhookConfig.Validation)
 		if err != nil {
-			return fmt.Errorf("failed to initialize webhook validators: %s", err)
+			return out, fmt.Errorf("failed to initialize webhook validators: %s", err)
 		}
 
 		addWebhookHandler := ancla.NewAddWebhookHandler(svc, ancla.HandlerConfig{
@@ -277,25 +301,36 @@ func webhookHandler(in webhookHandlerIn) error {
 	} else {
 		level.Info(in.Logger).Log(logging.MessageKey(), "Webhook service disabled")
 	}
-	return nil
+	return out, nil
 }
 
 func provideHandlers() fx.Option {
 	return fx.Options(
-		fx.Provide(authenticationProvider),
-		fx.Invoke(webhookHandler),
+		fx.Provide(
+			arrange.ProvideKey(authAcquirerKey, authAcquirerConfig{}),
+			arrange.UnmarshalKey(webhookConfigKey, ancla.Config{}),
+			arrange.ProvideKey("authHeader", []string{}),
+			arrange.UnmarshalKey("jwtValidator", JWTValidator{}),
+			arrange.UnmarshalKey("capabilityCheck", CapabilityConfig{}),
+			createAuthAcquirer,
+			provideAuthentication,
+		),
+		fx.Invoke(handleWebhooks),
 	)
 }
 
 type ServiceConfigIn struct {
 	fx.In
-	V                  viper.Viper
-	Logger             log.Logger
-	XmidtHTTPClient    *http.Client
-	XmidtClientTimeout httpClientTimeout `name:"xmidt_client_timeout"`
+	Logger               log.Logger
+	XmidtHTTPClient      *http.Client
+	XmidtClientTimeout   httpClientTimeout `name:"xmidt_client_timeout"`
+	RequestMaxRetries    int               `name:"requestMaxRetries"`
+	RequestRetryInterval time.Duration     `name:"requestRetryInterval"`
+	TargetURL            string            `name:"targetURL"`
+	WRPSource            string            `name:"WRPSource"`
 }
 
-func statServiceProvider(in ServiceConfigIn) *stat.ServiceOptions {
+func provideStatServiceOptions(in ServiceConfigIn) *stat.ServiceOptions {
 	//
 	// Stat Service configs
 	//
@@ -305,31 +340,31 @@ func statServiceProvider(in ServiceConfigIn) *stat.ServiceOptions {
 				Do: xhttp.RetryTransactor( //nolint:bodyclose
 					xhttp.RetryOptions{
 						Logger:   in.Logger,
-						Retries:  in.V.GetInt(reqMaxRetriesKey),
-						Interval: in.V.GetDuration(reqRetryIntervalKey),
+						Retries:  in.RequestMaxRetries,
+						Interval: in.RequestRetryInterval,
 					},
 					in.XmidtHTTPClient.Do),
 				RequestTimeout: in.XmidtClientTimeout.RequestTimeout,
 			}),
-		XmidtStatURL: fmt.Sprintf("%s/device/${device}/stat", in.V.GetString(targetURLKey)),
+		XmidtStatURL: fmt.Sprintf("%s/device/${device}/stat", in.TargetURL),
 	}
 }
 
-func translationOptionsProvider(in ServiceConfigIn) *translation.ServiceOptions {
+func provideTranslationOptions(in ServiceConfigIn) *translation.ServiceOptions {
 	//
 	// WRP Service configs
 	//
 	return &translation.ServiceOptions{
-		XmidtWrpURL: fmt.Sprintf("%s/device", in.V.GetString(targetURLKey)),
-		WRPSource:   in.V.GetString(wrpSourceKey),
+		XmidtWrpURL: fmt.Sprintf("%s/device", in.TargetURL),
+		WRPSource:   in.WRPSource,
 		T: transaction.New(
 			&transaction.Options{
 				RequestTimeout: in.XmidtClientTimeout.RequestTimeout,
 				Do: xhttp.RetryTransactor( //nolint:bodyclose
 					xhttp.RetryOptions{
 						Logger:   in.Logger,
-						Retries:  in.V.GetInt(reqMaxRetriesKey),
-						Interval: in.V.GetDuration(reqRetryIntervalKey),
+						Retries:  in.RequestMaxRetries,
+						Interval: in.RequestRetryInterval,
 					},
 					in.XmidtHTTPClient.Do),
 			}),
@@ -339,8 +374,22 @@ func translationOptionsProvider(in ServiceConfigIn) *translation.ServiceOptions 
 func provideServers() fx.Option {
 	return fx.Options(
 		fx.Provide(
-			statServiceProvider,
-			translationOptionsProvider,
+			arrange.ProvideKey(reqMaxRetriesKey, 0),
+			arrange.ProvideKey(reqRetryIntervalKey, time.Duration(0)),
+			arrange.ProvideKey("previousVersionSupport", true),
+			arrange.ProvideKey("targetURL", ""),
+			arrange.ProvideKey("WRPSource", ""),
+			arrange.ProvideKey(translationServicesKey, []string{}),
+			fx.Annotated{
+				Name:   "reducedLoggingResponseCodes",
+				Target: arrange.UnmarshalKey(reducedTransactionLoggingCodesKey, []int{}),
+			},
+			provideStatServiceOptions,
+			provideTranslationOptions,
+			fx.Annotated{
+				Name:   "api_router",
+				Target: provideAPIRouter,
+			},
 		),
 		arrangehttp.Server{
 			Name: "server_primary",
@@ -364,21 +413,24 @@ func provideServers() fx.Option {
 	)
 }
 
-type PrimaryRouterIn struct {
+type PrimaryEndpointIn struct {
 	fx.In
-	V                  *viper.Viper
-	Router             *mux.Router `name:"server_primary"`
-	APIBase            string      `name:"api_base"`
-	AuthChain          alice.Chain `name:"auth_chain"`
-	Tracing            candlelight.Tracing
-	Logger             log.Logger
-	StatServiceOptions *stat.ServiceOptions
-	TranslationOptions *translation.ServiceOptions
-	Authenticate       *alice.Chain
+	V                           *viper.Viper
+	Router                      *mux.Router `name:"server_primary"`
+	APIRouter                   *mux.Router `name:"api_router"`
+	APIBase                     string      `name:"api_base"`
+	AuthChain                   alice.Chain `name:"auth_chain"`
+	Tracing                     candlelight.Tracing
+	Logger                      log.Logger
+	StatServiceOptions          *stat.ServiceOptions
+	TranslationOptions          *translation.ServiceOptions
+	Authenticate                *alice.Chain
+	Acquirer                    acquire.Acquirer
+	ReducedLoggingResponseCodes []int    `name:"reducedLoggingResponseCodes"`
+	TranslationServices         []string `name:"supportedServices"`
 }
 
-func handlePrimaryEndpoint(in PrimaryRouterIn) *mux.Router {
-	rootRouter := mux.NewRouter()
+func handlePrimaryEndpoint(in PrimaryEndpointIn) {
 	otelMuxOptions := []otelmux.Option{
 		otelmux.WithPropagators(in.Tracing.Propagator()),
 		otelmux.WithTracerProvider(in.Tracing.TracerProvider()),
@@ -388,38 +440,46 @@ func handlePrimaryEndpoint(in PrimaryRouterIn) *mux.Router {
 		candlelight.EchoFirstTraceNodeInfo(in.Tracing.Propagator()),
 	)
 
-	APIRouter := rootRouter.PathPrefix(fmt.Sprintf("/%s/", apiBase)).Subrouter()
-
-	reducedLoggingResponseCodes := in.V.GetIntSlice(reducedTransactionLoggingCodesKey)
-
 	if in.V.IsSet(authAcquirerKey) {
-		acquirer, err := createAuthAcquirer(in.V)
-		if err != nil {
-			level.Error(in.Logger).Log(logging.MessageKey(), "Could not configure auth acquirer", logging.ErrorKey(), err)
-		} else {
-			in.TranslationOptions.AuthAcquirer = acquirer
-			in.StatServiceOptions.AuthAcquirer = acquirer
-			level.Info(in.Logger).Log(logging.MessageKey(), "Outbound request authentication token acquirer enabled")
-		}
+		acquirer := in.Acquirer
+		in.TranslationOptions.AuthAcquirer = acquirer
+		in.StatServiceOptions.AuthAcquirer = acquirer
+		level.Info(in.Logger).Log(logging.MessageKey(), "Outbound request authentication token acquirer enabled")
 	}
 	ss := stat.NewService(in.StatServiceOptions)
 	ts := translation.NewService(in.TranslationOptions)
+
 	// Must be called before translation.ConfigHandler due to mux path specificity (https://github.com/gorilla/mux#matching-routes).
 	stat.ConfigHandler(&stat.Options{
 		S:                           ss,
-		APIRouter:                   APIRouter,
+		APIRouter:                   in.APIRouter,
 		Authenticate:                in.Authenticate,
 		Log:                         in.Logger,
-		ReducedLoggingResponseCodes: reducedLoggingResponseCodes,
+		ReducedLoggingResponseCodes: in.ReducedLoggingResponseCodes,
 	})
-
 	translation.ConfigHandler(&translation.Options{
 		S:                           ts,
-		APIRouter:                   APIRouter,
+		APIRouter:                   in.APIRouter,
 		Authenticate:                in.Authenticate,
 		Log:                         in.Logger,
-		ValidServices:               in.V.GetStringSlice(translationServicesKey),
-		ReducedLoggingResponseCodes: reducedLoggingResponseCodes,
+		ValidServices:               in.TranslationServices,
+		ReducedLoggingResponseCodes: in.ReducedLoggingResponseCodes,
 	})
+}
+
+type APIRouterIn struct {
+	fx.In
+	PrevVerSupport bool `name:"previousVersionSupport"`
+}
+
+func provideAPIRouter(in APIRouterIn) *mux.Router {
+	rootRouter := mux.NewRouter()
+	// if we want to support the previous API version, then include it in the
+	// api base.
+	urlPrefix := fmt.Sprintf("/%s/", apiBase)
+	if in.PrevVerSupport {
+		urlPrefix = fmt.Sprintf("/%s/", apiBaseDualVersion)
+	}
+	APIRouter := rootRouter.PathPrefix(urlPrefix).Subrouter()
 	return APIRouter
 }
