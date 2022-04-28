9eada22ec3eaec94b303d86506014b76372a3aa7
mtrinh11 <mtrinh11@gmail.com>
2022-04-28 13:11:54 -0700

    added legal to top of files missing it (#274)

--- stat/endpoint_test.go
+++ stat/endpoint_test.go
@@ -1,3 +1,20 @@
+/**
+ * Copyright 2022 Comcast Cable Communications Management, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package stat
 
 import (
@@ -14,7 +31,7 @@ func TestMakeStatEndpoint(t *testing.T) {
 		AuthHeaderValue: "a0",
 	}
 
-	s.On("RequestStat", context.TODO(),"a0", "mac:1122334455").Return(nil, nil)
+	s.On("RequestStat", context.TODO(), "a0", "mac:1122334455").Return(nil, nil)
 
 	endpoint(context.TODO(), sr)
 	s.AssertExpectations(t)
--- translation/endpoint.go
+++ translation/endpoint.go
@@ -1,3 +1,20 @@
+/**
+ * Copyright 2022 Comcast Cable Communications Management, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package translation
 
 import (
@@ -15,6 +32,6 @@ type wrpRequest struct {
 func makeTranslationEndpoint(s Service) endpoint.Endpoint {
 	return func(ctx context.Context, request interface{}) (interface{}, error) {
 		wrpReq := (request).(*wrpRequest)
-		return s.SendWRP(ctx,wrpReq.WRPMessage, wrpReq.AuthHeaderValue)
+		return s.SendWRP(ctx, wrpReq.WRPMessage, wrpReq.AuthHeaderValue)
 	}
 }
