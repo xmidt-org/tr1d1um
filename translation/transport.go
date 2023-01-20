c47174cd49084acf23eb746065da90b5232d5174
Owen Cabalceta <denopink@gmail.com>
2023-01-20 11:30:49 -0500

    update docs

--- translation/transport.go
+++ translation/transport.go
@@ -210,7 +210,7 @@ func encodeResponse(ctx context.Context, w http.ResponseWriter, response interfa
 
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
-		// use the device response status code if it's within 520-599 inclusive
+		// use the device response status code if it's within 520-599 (inclusive)
 		// https://github.com/xmidt-org/tr1d1um/issues/354
 		if errUnmarshall := json.Unmarshal(wrpModel.Payload, &d); errUnmarshall == nil {
 			if 520 <= d.StatusCode && d.StatusCode <= 599 {
